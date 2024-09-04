375ae0a871fb69162c04439c5585936a9c8d2ca5
Hao Chen <55617979+chqwer2@users.noreply.github.com>
2024-09-04 23:05:03 +0800

    Create brain_seperate.py

---
+++ process/brain_seperate.py
@@ -0,0 +1,40 @@
+from glob import glob
+import shutil, os
+
+root = "/data"
+# save to "root/Brain
+
+TASK = "GLI"
+
+segfiles = sorted(glob(os.path.join(root, f"ASNR-MICCAI-BraTS2023-{TASK}-Challenge-TrainingData/*/*-seg.*")))
+
+
+modalities = ["t1c", "t1n", "t2w", "t2f"]
+
+
+for file in segfiles:
+    for modality in modalities:
+
+        imgfile = file.replace("seg",  modality)
+        savefilename = os.path.basename(file).replace("-seg", "")
+
+
+        savepath = os.path.join(root, f"Brain/BraTS-{TASK}-{modality.upper()}")
+        os.makedirs(savepath, exist_ok=True)
+
+        imgpath = os.path.join(savepath, "img")
+        os.makedirs(imgpath, exist_ok=True)
+        segpath = os.path.join(savepath, "seg")
+        os.makedirs(segpath, exist_ok=True)
+
+        print("from ", imgfile)
+        print("save to ", os.path.join(imgpath, savefilename))
+
+        shutil.copy(imgfile, os.path.join(imgpath, savefilename))
+        shutil.copy(file, os.path.join(segpath, savefilename))
+
+
+
+
+
+# ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData/BraTS-GLI-00117-000/BraTS-GLI-00117-000-t2w.nii.gz
