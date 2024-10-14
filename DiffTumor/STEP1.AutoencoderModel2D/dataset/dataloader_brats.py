1119272555b38edf7ca8e799bb801f3d41089f7f
Hao Chen <55617979+chqwer2@users.noreply.github.com>
2024-10-15 00:05:33 +0800

    Update dataloader_brats.py

--- DiffTumor/STEP1.AutoencoderModel2D/dataset/dataloader_brats.py
+++ DiffTumor/STEP1.AutoencoderModel2D/dataset/dataloader_brats.py
@@ -182,7 +182,7 @@ def get_loader(args, splits=[0.7, 0.1, 0.2]):
     else:  
         loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=4, collate_fn=list_data_collate)
     
-    return loader, transform, len(dataset)
+    return loader, transform  #, len(dataset)
     
     
 
