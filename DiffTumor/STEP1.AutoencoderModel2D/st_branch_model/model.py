50c2c2082c0b09e257c93f3816f05669c51d232a
Hao Chen <55617979+chqwer2@users.noreply.github.com>
2024-10-15 03:48:12 +0800

    Update model.py

--- DiffTumor/STEP1.AutoencoderModel2D/st_branch_model/model.py
+++ DiffTumor/STEP1.AutoencoderModel2D/st_branch_model/model.py
@@ -25,8 +25,9 @@ class SiLU(nn.Module):
     
     
 class ModelBackbone(nn.Module):
-    def __init__(self, args) -> None:
+    def __init__(self, args):
         super(ModelBackbone).__init__()
+        self.args = args
         num_every_group = args.model.base_num_every_group
         
         self.init_T2_frq_branch(args)
