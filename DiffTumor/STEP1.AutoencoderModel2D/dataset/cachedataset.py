c3035304a8ec2a8fb9d4ae02e136426d80fcfeae
Hao Chen <55617979+chqwer2@users.noreply.github.com>
2024-10-15 18:00:18 +0800

    Update cachedataset.py

--- DiffTumor/STEP1.AutoencoderModel2D/dataset/cachedataset.py
+++ DiffTumor/STEP1.AutoencoderModel2D/dataset/cachedataset.py
@@ -53,15 +53,16 @@ class PatchIterd:
             original_spatial_shape = d[first(self.keys)].shape[1:]
             # Filter Zero Slices
             print("original_spatial_shape", d[first(self.keys)].shape) 
+            # original_spatial_shape torch.Size([1, 128, 128, 96])
             # original_spatial_shape torch.Size([128, 128, 96])
-            print("original type:", d[first(self.keys)].type())
+            # print("original type:", d[first(self.keys)].type())
             
             # Bugs
-            filter = torch.any(torch.any(d[first(self.keys)][0], dim=0), dim=1)
-            print("filter size:", filter.shape)
+            filter = torch.any(torch.any(d[first(self.keys)][0], dim=0), dim=0)
+            # print("filter size:", filter.shape)
             
             for key in self.keys:
-                print("key:", key)
+                # print("key:", key)
                 d[key] = d[key][..., filter]
             original_spatial_shape = d[first(self.keys)].shape[1:]
             print("new original_spatial_shape", original_spatial_shape)
