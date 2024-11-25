ba36a9ca159065fd2e125f6e45542c9bc8e32614
Hao Chen <55617979+chqwer2@users.noreply.github.com>
2024-11-25 08:08:31 +0800

    Update degradation.py

--- Cold-Diffusion/defading-diffusion-pytorch/defading_diffusion_pytorch/degradation.py
+++ Cold-Diffusion/defading-diffusion-pytorch/defading_diffusion_pytorch/degradation.py
@@ -145,27 +145,19 @@ def get_ksu_kernel(timesteps, image_size,
         sr_list = sr_list #  accelerated_factor
 
         af = 1 / sr_list[-1]
-        cf = sr_list[0] * 0.32
-        if use_fix_center_ratio:
-            cf = 0.1
+        cf = 0.1 if use_fix_center_ratio else sr_list[0] * 0.32
 
-        print("sr= ", sr_list[-1])
+        # print("sr= ", sr_list[-1])
 
         # Full
         cache_mask = get_ksu_mask(mask_method, af, cf, pe=ksu_mask_pe, fe=ksu_mask_fe)
         masks.append(cache_mask)
 
-        print("cache_mask shape: ", cache_mask.shape, cache_mask.sum())
         sr_list = sr_list[:-1]
         sr_list = sr_list.flip(0)
 
         for sr in sr_list:
-            print("sr= ", sr)
             af = 1 / sr
-            cf = sr * 0.32
-            if use_fix_center_ratio:
-                cf = 0.1
-
             H, W = cache_mask.shape[1], cache_mask.shape[2]
             new_mask = cache_mask.clone()
 
@@ -173,22 +165,18 @@ def get_ksu_kernel(timesteps, image_size,
             total_lines = H
             sampled_lines = int(total_lines / af)
             existing_lines = new_mask.squeeze(0).sum(dim=0).nonzero(as_tuple=True)[0].tolist()
-            print("existing_lines = ", len(existing_lines))
-            print("sampled_lines = ", sampled_lines)
 
             remaining_lines = [i for i in range(total_lines) if i not in existing_lines]
 
             if sampled_lines > len(existing_lines):
                 additional_lines = sampled_lines - len(existing_lines)
-                print("additional_lines = ", additional_lines)
                 sampled_indices = np.random.choice(remaining_lines, additional_lines, replace=False)
                 new_mask[:, :, sampled_indices] = 1.0
-
 
             cache_mask = new_mask
             masks.append(cache_mask)
 
-            print("cache_mask shape: ", cache_mask.sum())
+            # print("cache_mask shape: ", cache_mask.sum())
 
 
 
